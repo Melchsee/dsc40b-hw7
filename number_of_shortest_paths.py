dea6ecc9598c309608565b92c1596d888e2d3fdc
BlackwingYu <cedricwong0@gmail.com>
2020-05-21 23:42:53 -0700

    added parentheses

--- number_of_shortest_paths.py
+++ number_of_shortest_paths.py
@@ -20,7 +20,7 @@ def number_of_shortest_paths(graph, source):
                 predecessor[v] = u
                 pending.append(v)
                 shortest_paths[v] = 1
-            if status[v] == 'pending' and distance(v) == distance[u] + 1:
+            if status[v] == 'pending' and distance(v) == (distance[u] + 1):
                 shortest_paths[v] += 1
         status[u] = 'visited'
 
